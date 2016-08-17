84903f8cd38f52aa82ec83c724b0dc33a52a3743
Andrew Nadolski <andrew.nadolski@gmail.com>
2016-08-17 00:05:03 -0500

    start to implement the primary reflection and transmission calculation function

--- c_artools/simulator.c
+++ c_artools/simulator.c
@@ -3,6 +3,8 @@
 #include <complex.h>
 #include <stdlib.h>
 
+#define NUM_LAYERS 5
+
 /* The main mathematical functionality of the simulator.py module written in C. 
 Just for shits and giggles. And learning C.
 
@@ -13,8 +15,8 @@ talk to one another.*/
 // function prototyping
 
 double get_index(double dielectric);
-double _Complex find_k(double index, double freq, double loss);
-double _Complex find_k_offset(double _Complex k, double d);
+double _Complex find_k(double frequency, double index, double loss, double thickness);
+//double _Complex find_k_offset(double _Complex k, double d);
 double r_at_interface(double n1, double n2);
 double t_at_interface(double n1, double n2);
 double ** make_2x2(void);
@@ -22,164 +24,169 @@ double _Complex ** make_complex_2x2(void);
 double _Complex *** make_complex_nd(int n);
 double get_R(double _Complex r_amp);
 double get_T(double _Complex t_amp, double n_i, double n_f);
+double _Complex * calc_RT_amp(double frequency, double dielectric[NUM_LAYERS], 
+			      double loss[NUM_LAYERS], double thickness[NUM_LAYERS]);
 
 // main, which is really the debugging portion
 
 int main() {
   printf("\n#####################\n## BEGIN DEBUGGING ##\n#####################\n\n");
 
-  double dielectric1;
-  double dielectric2;
-  double index1;
-  double index2;
-  dielectric1 = 4;
-  dielectric2 = 7;
-  printf("Test 'get_index'\n");
-  index1 = get_index(dielectric1);
-  index2 = get_index(dielectric2);
-  printf("'get_index' result is %lf\n\n", index1);
+/*   double dielectric1; */
+/*   double dielectric2; */
+/*   double index1; */
+/*   double index2; */
+/*   dielectric1 = 4; */
+/*   dielectric2 = 7; */
+/*   printf("Test 'get_index'\n"); */
+/*   index1 = get_index(dielectric1); */
+/*   index2 = get_index(dielectric2); */
+/*   printf("'get_index' result is %lf\n\n", index1); */
 
-  double freq;
-  double loss;
-  double _Complex k;
-  freq = 150E9;
-  loss = 0.00025;
-  printf("Test 'find_k'\n");
-  k = find_k(index1, freq, loss);
-  printf("'find_k' result is %lf %lfi\n\n", creal(k), cimag(k));
+/*   double freq; */
+/*   double loss; */
+/*   double _Complex k; */
+/*   freq = 150E9; */
+/*   loss = 0.00025; */
+/*   printf("Test 'find_k'\n"); */
+/*   k = find_k(index1, freq, loss); */
+/*   printf("'find_k' result is %lf %lfi\n\n", creal(k), cimag(k)); */
   
-  double d;
-  double _Complex delta;
-  d = 15.0*2.54E-5;
-  printf("Test 'find_k_offset'\n");
-  delta = find_k_offset(k, d);
-  printf("'find_k_offset' result is %lf %lfi\n\n", creal(delta), cimag(delta));
+/*   double d; */
+/*   double _Complex delta; */
+/*   d = 15.0*2.54E-5; */
+/*   printf("Test 'find_k_offset'\n"); */
+/*   delta = find_k_offset(k, d); */
+/*   printf("'find_k_offset' result is %lf %lfi\n\n", creal(delta), cimag(delta)); */
   
-  double r;
-  printf("Test 'r_at_interface'\n");
-  r = r_at_interface(index1, index2);
-  printf("'r_at_interface' result is %lf\n\n", r);
+/*   double r; */
+/*   printf("Test 'r_at_interface'\n"); */
+/*   r = r_at_interface(index1, index2); */
+/*   printf("'r_at_interface' result is %lf\n\n", r); */
 
-  double t;
-  printf("Test 't_at_interface'\n");
-  t = t_at_interface(index1, index2);
-  printf("'t_at_interface' result is %lf\n\n", t);
+/*   double t; */
+/*   printf("Test 't_at_interface'\n"); */
+/*   t = t_at_interface(index1, index2); */
+/*   printf("'t_at_interface' result is %lf\n\n", t); */
 
-  double ** matrix;
-  printf("Test 'make_2x2'\n");
-  matrix = make_2x2();
-  printf("'make_2x2' result is:\n %lf %lf\n %lf %lf\n\n",
-	 matrix[0][0], matrix[0][1],
-	 matrix[1][0], matrix[1][1]);
-  free(matrix);
+/*   double ** matrix; */
+/*   printf("Test 'make_2x2'\n"); */
+/*   matrix = make_2x2(); */
+/*   printf("'make_2x2' result is:\n %lf %lf\n %lf %lf\n\n", */
+/* 	 matrix[0][0], matrix[0][1], */
+/* 	 matrix[1][0], matrix[1][1]); */
+/*   free(matrix); */
 
-  double _Complex ** c_matrix;
-  printf("Test 'make_complex_2x2'\n");
-  c_matrix = make_complex_2x2();
-  printf("'make_complex_2x2' result is:\n%lf+%lfi  %lf+%lfi\n%lf+%lfi  %lf+%lfi\n\n",
-	 creal(c_matrix[0][0]), cimag(c_matrix[0][0]),
-	 creal(c_matrix[0][1]), cimag(c_matrix[0][1]),
-	 creal(c_matrix[1][0]), cimag(c_matrix[1][0]),
-	 creal(c_matrix[1][1]), cimag(c_matrix[1][1]));
+/*   double _Complex ** c_matrix; */
+/*   printf("Test 'make_complex_2x2'\n"); */
+/*   c_matrix = make_complex_2x2(); */
+/*   printf("'make_complex_2x2' result is:\n%lf+%lfi  %lf+%lfi\n%lf+%lfi  %lf+%lfi\n\n", */
+/* 	 creal(c_matrix[0][0]), cimag(c_matrix[0][0]), */
+/* 	 creal(c_matrix[0][1]), cimag(c_matrix[0][1]), */
+/* 	 creal(c_matrix[1][0]), cimag(c_matrix[1][0]), */
+/* 	 creal(c_matrix[1][1]), cimag(c_matrix[1][1])); */
 
-  printf("Rewrite 'make_complex_2x2' values\n");
-  c_matrix[0][0] = 42.0 - 2.0*I;
-  c_matrix[0][1] = 22.43 + 9.0*I;
-  c_matrix[1][0] = 1.0 + 33.0*I;
-  c_matrix[1][1] = cexp(5.0+7777.7*I);
-  printf("Rewrite 'make_complex_2x2' result is:\n%lf+%lfi  %lf+%lfi\n%lf+%lfi  %lf+%lfi\n\n",
-	 creal(c_matrix[0][0]), cimag(c_matrix[0][0]),
-	 creal(c_matrix[0][1]), cimag(c_matrix[0][1]),
-	 creal(c_matrix[1][0]), cimag(c_matrix[1][0]),
-	 creal(c_matrix[1][1]), cimag(c_matrix[1][1]));
-  free(c_matrix);
+/*   printf("Rewrite 'make_complex_2x2' values\n"); */
+/*   c_matrix[0][0] = 42.0 - 2.0*I; */
+/*   c_matrix[0][1] = 22.43 + 9.0*I; */
+/*   c_matrix[1][0] = 1.0 + 33.0*I; */
+/*   c_matrix[1][1] = cexp(5.0+7777.7*I); */
+/*   printf("Rewrite 'make_complex_2x2' result is:\n%lf+%lfi  %lf+%lfi\n%lf+%lfi  %lf+%lfi\n\n", */
+/* 	 creal(c_matrix[0][0]), cimag(c_matrix[0][0]), */
+/* 	 creal(c_matrix[0][1]), cimag(c_matrix[0][1]), */
+/* 	 creal(c_matrix[1][0]), cimag(c_matrix[1][0]), */
+/* 	 creal(c_matrix[1][1]), cimag(c_matrix[1][1])); */
+/*   free(c_matrix); */
 
-  double _Complex r_amp;
-  double R;
-  r_amp = 2.5 + 4.0*I;
-  printf("Test 'get_R'\n");
-  R = get_R(r_amp);
-  printf("The result of 'get_R' is %lf\n\n", R);
+/*   double _Complex r_amp; */
+/*   double R; */
+/*   r_amp = 2.5 + 4.0*I; */
+/*   printf("Test 'get_R'\n"); */
+/*   R = get_R(r_amp); */
+/*   printf("The result of 'get_R' is %lf\n\n", R); */
 
-  double _Complex t_amp;
-  double T;
-  double n_i;
-  double n_f;
-  n_i = 1.0;
-  n_f = 9.7;
-  t_amp = 258. + 77.0*I;
-  printf("Test 'get_T'\n");
-  T = get_T(t_amp, n_i, n_f);
-  printf("The result of 'get_T' is %lf\n\n", T);
+/*   double _Complex t_amp; */
+/*   double T; */
+/*   double n_i; */
+/*   double n_f; */
+/*   n_i = 1.0; */
+/*   n_f = 9.7; */
+/*   t_amp = 258. + 77.0*I; */
+/*   printf("Test 'get_T'\n"); */
+/*   T = get_T(t_amp, n_i, n_f); */
+/*   printf("The result of 'get_T' is %lf\n\n", T); */
 
-  printf("Test adding stuff to an array\n");
-  int array[100];
-  int count;
-  array[0] = 42;
-  count = 0;
-  printf("The value in array[0] is %d\n\n", array[0]);
+/*   printf("Test adding stuff to an array\n"); */
+/*   int array[100]; */
+/*   int count; */
+/*   array[0] = 42; */
+/*   count = 0; */
+/*   printf("The value in array[0] is %d\n\n", array[0]); */
   
-  double _Complex *** c_array_3d;
-  int n;
-  n = 3;
-  printf("Test 'make_complex_nd' with dimension 3\n");
-  c_array_3d = make_complex_nd(n);
-  printf("Slice 1:\n%lf+%lfi %lf+%lfi\n%lf+%lfi %lf+%lfi\n",
-	 creal(c_array_3d[0][0][0]), cimag(c_array_3d[0][0][0]),
-	 creal(c_array_3d[0][0][1]), cimag(c_array_3d[0][0][1]),
-	 creal(c_array_3d[0][1][0]), cimag(c_array_3d[0][1][0]),
-	 creal(c_array_3d[0][1][1]), cimag(c_array_3d[0][1][1]));
-  printf("Slice 2:\n%lf+%lfi %lf+%lfi\n%lf+%lfi %lf+%lfi\n",
-	 creal(c_array_3d[1][0][0]), cimag(c_array_3d[1][0][0]),
-	 creal(c_array_3d[1][0][1]), cimag(c_array_3d[1][0][1]),
-	 creal(c_array_3d[1][1][0]), cimag(c_array_3d[1][1][0]),
-	 creal(c_array_3d[1][1][1]), cimag(c_array_3d[1][1][1]));
-  printf("Slice 3:\n%lf+%lfi %lf+%lfi\n%lf+%lfi %lf+%lfi\n",
-	 creal(c_array_3d[2][0][0]), cimag(c_array_3d[2][0][0]),
-	 creal(c_array_3d[2][0][1]), cimag(c_array_3d[2][0][1]),
-	 creal(c_array_3d[2][1][0]), cimag(c_array_3d[2][1][0]),
-	 creal(c_array_3d[2][1][1]), cimag(c_array_3d[2][1][1]));
+/*   double _Complex *** c_array_3d; */
+/*   int n; */
+/*   n = 3; */
+/*   printf("Test 'make_complex_nd' with dimension 3\n"); */
+/*   c_array_3d = make_complex_nd(n); */
+/*   printf("Slice 1:\n%lf+%lfi %lf+%lfi\n%lf+%lfi %lf+%lfi\n", */
+/* 	 creal(c_array_3d[0][0][0]), cimag(c_array_3d[0][0][0]), */
+/* 	 creal(c_array_3d[0][0][1]), cimag(c_array_3d[0][0][1]), */
+/* 	 creal(c_array_3d[0][1][0]), cimag(c_array_3d[0][1][0]), */
+/* 	 creal(c_array_3d[0][1][1]), cimag(c_array_3d[0][1][1])); */
+/*   printf("Slice 2:\n%lf+%lfi %lf+%lfi\n%lf+%lfi %lf+%lfi\n", */
+/* 	 creal(c_array_3d[1][0][0]), cimag(c_array_3d[1][0][0]), */
+/* 	 creal(c_array_3d[1][0][1]), cimag(c_array_3d[1][0][1]), */
+/* 	 creal(c_array_3d[1][1][0]), cimag(c_array_3d[1][1][0]), */
+/* 	 creal(c_array_3d[1][1][1]), cimag(c_array_3d[1][1][1])); */
+/*   printf("Slice 3:\n%lf+%lfi %lf+%lfi\n%lf+%lfi %lf+%lfi\n", */
+/* 	 creal(c_array_3d[2][0][0]), cimag(c_array_3d[2][0][0]), */
+/* 	 creal(c_array_3d[2][0][1]), cimag(c_array_3d[2][0][1]), */
+/* 	 creal(c_array_3d[2][1][0]), cimag(c_array_3d[2][1][0]), */
+/* 	 creal(c_array_3d[2][1][1]), cimag(c_array_3d[2][1][1])); */
 
-  // make a mock simulation using the inputs:
-  // thickness, dielectric constant, and loss tangent
+/*   // make a mock simulation using the inputs: */
+/*   // thickness, dielectric constant, and loss tangent */
 
   printf("\n>>>> Testing grabbing from arrays and calculating things, then putting values back into new arrays. <<<<\n\n");
+  double freq;
+  double dielectric[NUM_LAYERS] = {1.0, 2.4, 3.15, 6.15, 9.7};
+  double thickness[NUM_LAYERS] = {1000.0, 15.0*2.54E-5, 5.0*2.54E-5, 5.0*2.54E-5, 1000.0};
+  double loss[NUM_LAYERS] = {0.0, 2.5E-4, 1.7E-3, 1.526E-3, 7.4E-4};
 
-  double dielectrics[5] = {1.0, 2.4, 3.15, 6.15, 9.7};
-  double thicknesses[5] = {1000.0, 15.0*2.54E-5, 5.0*2.54E-5, 5.0*2.54E-5, 1000.0};
-  double losses[5] = {0.0, 2.5E-4, 1.7E-3, 1.526E-3, 7.4E-4};
+  freq = 150E9;
+/*   double _Complex ks[NUM_LAYERS]; */
+/*   double deltas[NUM_LAYERS]; */
+/*   double index[NUM_LAYERS]; */
+/*   // int count; */
+/*   int i; */
 
-  double _Complex ks[5];
-  double deltas[5];
-  double index[5];
-  // int count;
-  int i;
+/*   printf("Test converting the array of dielectric constants to refractive indices.\n"); */
+/*   for (i=0; i<NUM_LAYERS; i++) { */
+/*     index[i] = get_index(dielectrics[i]); */
+/*   } */
+/*   printf("The conversion from dielectric to index is:\n"); */
+/*   for (i=0; i<NUM_LAYERS; i++) { */
+/*     printf("%lf ---> %lf\n", dielectrics[i], index[i]); */
+/*   } */
 
-  printf("Test converting the array of dielectric constants to refractive indices.\n");
-  for (i=0; i<5; i++) {
-    index[i] = get_index(dielectrics[i]);
-  }
-  printf("The conversion from dielectric to index is:\n");
-  for (i=0; i<5; i++) {
-    printf("%lf ---> %lf\n", dielectrics[i], index[i]);
-  }
+/*   printf("\nTest calculating the wavenumbers.\n"); */
+/*   for (i=0; i<NUM_LAYERS; i++) { */
+/*     ks[i] = find_k(index[i], freq, losses[i]); */
+/*   } */
+/*   printf("The dimensioned wavenumber (with units m^(-1)) for each layer is:\n"); */
+/*   for (i=0; i<NUM_LAYERS; i++) { */
+/*     printf("%lf+%lfi\n", creal(ks[i]), cimag(ks[i])); */
+/*   } */
+/*   for (i=0; i<NUM_LAYERS; i++) { */
+/*     deltas[i] = ks[i]*thicknesses[i]; */
+/*   } */
+/*   printf("\nAnd the dimensionless wavenumber for each layer is:\n"); */
+/*   for (i=0; i<NUM_LAYERS; i++) { */
+/*     printf("%lf+%lf\n", creal(deltas[i]), cimag(deltas[i])); */
+/*   } */
 
-  printf("\nTest calculating the wavenumbers.\n");
-  for (i=0; i<5; i++) {
-    ks[i] = find_k(index[i], freq, losses[i]);
-  }
-  printf("The dimensioned wavenumber (with units m^(-1)) for each layer is:\n");
-  for (i=0; i<5; i++) {
-    printf("%lf+%lfi\n", creal(ks[i]), cimag(ks[i]));
-  }
-  for (i=0; i<5; i++) {
-    deltas[i] = ks[i]*thicknesses[i];
-  }
-  printf("\nAnd the dimensionless wavenumber for each layer is:\n");
-  for (i=0; i<5; i++) {
-    printf("%lf+%lf\n", creal(deltas[i]), cimag(deltas[i]));
-  }
-    
+  double _Complex * test_val;
+  test_val = calc_RT_amp(freq, dielectric, loss, thickness);
 
   printf("\n#####################\n### END DEBUGGING ###\n#####################\n\n");
 }
@@ -188,8 +195,8 @@ double get_index(double dielectric) {
   return sqrt(dielectric);
 }
 
-double _Complex find_k(double index, double freq, double loss) {
-  return (2*M_PI*index*freq*(1-0.5*loss*I))/3E8;
+double _Complex find_k(double frequency, double index, double loss, double thickness) {
+  return (2*M_PI*index*frequency*thickness*(1-0.5*loss*I))/3E8;
 }
 
 double _Complex find_k_offset(double _Complex k, double d) {
@@ -263,4 +270,82 @@ double get_T(double _Complex t_amp, double n_i, double n_f) {
   // but maybe that isn't correct? second guessing myself here...
   return fabs(pow(t_amp, 2))*(n_f/n_i);
   //return pow(cabs(t_amp), 2)*(n_f/n_i);
+}
+
+double _Complex * calc_RT_amp(double frequency, double dielectric[NUM_LAYERS], double loss[NUM_LAYERS], double thickness[NUM_LAYERS]) {
+  double index[NUM_LAYERS];
+  double _Complex delta[NUM_LAYERS];
+  double _Complex r_amp[NUM_LAYERS-1][NUM_LAYERS-1] = {0};
+  double _Complex t_amp[NUM_LAYERS-1][NUM_LAYERS-1] = {0};
+  double _Complex * RT_amp;
+  int i,j,k;
+
+  printf("\n#### Inside 'calc_RT_amp'. ####\n");
+
+  RT_amp = (double _Complex *)malloc(sizeof(double _Complex)*2);
+
+  for (i=0; i<NUM_LAYERS; i++) {
+    index[i] = get_index(dielectric[i]);
+    delta[i] = find_k(frequency, index[i], loss[i], thickness[i]);
+  }
+
+  // print statements for debugging
+  printf("\nHere are the refractive indices calculated from the dielectric constants passed to the function.\n");
+  for (i=0; i<NUM_LAYERS; i++) {
+    printf("%lf\n", index[i]);
+  }
+  printf("\nHere are the deltas calculated from the input frequency, indices, losses, and thicknesses.\n");
+  for (i=0; i<NUM_LAYERS; i++) {
+    printf("%lf+%lfi\n", creal(delta[i]), cimag(delta[i]));
+  }
+  printf("\nHere are the original values of 'r_amp' and 't_amp'.\n");
+  printf("r_amp:\n%lf+%lfi %lf+%lfi %lf+%lfi %lf+%lfi\n%lf+%lfi %lf+%lfi %lf+%lfi %lf+%lfi\n%lf+%lfi %lf+%lfi %lf+%lfi %lf+%lfi\n%lf+%lfi %lf+%lfi %lf+%lfi %lf+%lfi\n\n",
+	 creal(r_amp[0][0]), cimag(r_amp[0][0]), creal(r_amp[0][1]), cimag(r_amp[0][1]), creal(r_amp[0][2]), cimag(r_amp[0][2]),
+	 creal(r_amp[0][3]), cimag(r_amp[0][3]),
+	 creal(r_amp[1][0]), cimag(r_amp[1][0]), creal(r_amp[1][1]), cimag(r_amp[1][1]), creal(r_amp[1][2]), cimag(r_amp[1][2]),
+         creal(r_amp[1][3]), cimag(r_amp[1][3]),
+	 creal(r_amp[2][0]), cimag(r_amp[2][0]), creal(r_amp[2][1]), cimag(r_amp[2][1]), creal(r_amp[2][1]), cimag(r_amp[2][1]),
+	 creal(r_amp[2][3]), cimag(r_amp[2][3]),
+	 creal(r_amp[3][0]), cimag(r_amp[3][0]) ,creal(r_amp[3][1]), cimag(r_amp[3][1]), creal(r_amp[3][2]), cimag(r_amp[3][2]), 
+	 creal(r_amp[3][3]), cimag(r_amp[3][3]));
+
+  printf("t_amp:\n%lf+%lfi %lf+%lfi %lf+%lfi %lf+%lfi\n%lf+%lfi %lf+%lfi %lf+%lfi %lf+%lfi\n%lf+%lfi %lf+%lfi %lf+%lfi %lf+%lfi\n%lf+%lfi %lf+%lfi %lf+%lfi %lf+%lfi\n\n",
+         creal(t_amp[0][0]), cimag(t_amp[0][0]), creal(t_amp[0][1]), cimag(t_amp[0][1]), creal(t_amp[0][2]), cimag(t_amp[0][2]),
+         creal(t_amp[0][3]), cimag(t_amp[0][3]),
+         creal(t_amp[1][0]), cimag(t_amp[1][0]), creal(t_amp[1][1]), cimag(t_amp[1][1]), creal(t_amp[1][2]), cimag(t_amp[1][2]),     
+         creal(t_amp[1][3]), cimag(t_amp[1][3]),
+         creal(t_amp[2][0]), cimag(t_amp[2][0]), creal(t_amp[2][1]), cimag(t_amp[2][1]), creal(t_amp[2][1]), cimag(t_amp[2][1]),
+         creal(t_amp[2][3]), cimag(t_amp[2][3]),
+         creal(t_amp[3][0]), cimag(t_amp[3][0]) ,creal(t_amp[3][1]), cimag(t_amp[3][1]), creal(t_amp[3][2]), cimag(t_amp[3][2]),
+         creal(t_amp[3][3]), cimag(t_amp[3][3]));
+
+  for (i=1; i<NUM_LAYERS-1; i++) {
+    r_amp[i][i+1] = r_at_interface(index[i], index[i+1]);
+    t_amp[i][i+1] = t_at_interface(index[i], index[i+1]);
+  }
+  printf("\nHere are the modified values of 'r_amp' and 't_amp'.\n");
+  printf("r_amp:\n%lf+%lfi %lf+%lfi %lf+%lfi %lf+%lfi\n%lf+%lfi %lf+%lfi %lf+%lfi %lf+%lfi\n%lf+%lfi %lf+%lfi %lf+%lfi %lf+%lfi\n%lf+\
+%lfi %lf+%lfi %lf+%lfi %lf+%lfi\n\n",
+         creal(r_amp[0][0]), cimag(r_amp[0][0]), creal(r_amp[0][1]), cimag(r_amp[0][1]), creal(r_amp[0][2]), cimag(r_amp[0][2]),
+         creal(r_amp[0][3]), cimag(r_amp[0][3]),
+         creal(r_amp[1][0]), cimag(r_amp[1][0]), creal(r_amp[1][1]), cimag(r_amp[1][1]), creal(r_amp[1][2]), cimag(r_amp[1][2]),
+         creal(r_amp[1][3]), cimag(r_amp[1][3]),
+         creal(r_amp[2][0]), cimag(r_amp[2][0]), creal(r_amp[2][1]), cimag(r_amp[2][1]), creal(r_amp[2][1]), cimag(r_amp[2][1]),
+         creal(r_amp[2][3]), cimag(r_amp[2][3]),
+         creal(r_amp[3][0]), cimag(r_amp[3][0]) ,creal(r_amp[3][1]), cimag(r_amp[3][1]), creal(r_amp[3][2]), cimag(r_amp[3][2]),
+         creal(r_amp[3][3]), cimag(r_amp[3][3]));
+
+  printf("t_amp:\n%lf+%lfi %lf+%lfi %lf+%lfi %lf+%lfi\n%lf+%lfi %lf+%lfi %lf+%lfi %lf+%lfi\n%lf+%lfi %lf+%lfi %lf+%lfi %lf+%lfi\n%lf+\
+%lfi %lf+%lfi %lf+%lfi %lf+%lfi\n\n",
+         creal(t_amp[0][0]), cimag(t_amp[0][0]), creal(t_amp[0][1]), cimag(t_amp[0][1]), creal(t_amp[0][2]), cimag(t_amp[0][2]),
+         creal(t_amp[0][3]), cimag(t_amp[0][3]),
+         creal(t_amp[1][0]), cimag(t_amp[1][0]), creal(t_amp[1][1]), cimag(t_amp[1][1]), creal(t_amp[1][2]), cimag(t_amp[1][2]),
+         creal(t_amp[1][3]), cimag(t_amp[1][3]),
+         creal(t_amp[2][0]), cimag(t_amp[2][0]), creal(t_amp[2][1]), cimag(t_amp[2][1]), creal(t_amp[2][1]), cimag(t_amp[2][1]),
+         creal(t_amp[2][3]), cimag(t_amp[2][3]),
+         creal(t_amp[3][0]), cimag(t_amp[3][0]) ,creal(t_amp[3][1]), cimag(t_amp[3][1]), creal(t_amp[3][2]), cimag(t_amp[3][2]),
+         creal(t_amp[3][3]), cimag(t_amp[3][3]));
+
+  RT_amp[0] = -1.0-1.0*I;
+  return RT_amp;
 }
