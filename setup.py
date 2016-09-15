7c3408f2b13e51fe96a32e72b7c8392e4c071760
Andrew Nadolski <andrew.nadolski@gmail.com>
2016-09-15 10:10:25 -0500

    add the first iteration of the setup file

---
+++ setup.py
@@ -0,0 +1,34 @@
+from distutils.core import setup
+
+setup(
+    # Application name:
+    name = "artools",
+
+    # Version number:
+    version = "0.1.1",
+
+    # Application author details:
+    author = "Andrew Nadolski",
+    author_email = "andrew.nadolski@gmail.com",
+
+    # Packages:
+    packages = ['artools'],
+
+    # Include additional files in the package:
+    include_package_data = True,
+
+    # Details:
+    url = "https://github.com/anadolsk1/artools",
+
+    # License:
+    license = "LICENSE.txt",
+
+    # Description:
+    description = "Placeholder description. See README?",
+
+    # Dependencies:
+    install_requires = [
+        "numpy",
+        "matplotlib",
+    ],
+)
